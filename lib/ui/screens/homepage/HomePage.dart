4be54b75e77bcfae123874e6e4409b5853fec967
Rahatul Aseaikin Shovo <27925309+rahat14@users.noreply.github.com>
2024-03-27 05:02:46 +0600

    Home Page Code Clean-up

--- lib/ui/screens/homepage/HomePage.dart
+++ lib/ui/screens/homepage/HomePage.dart
@@ -1,6 +1,5 @@
-import 'dart:io';
-
 import 'package:cheq/ui/screens/homepage/controller/HomeScreenController.dart';
+import 'package:cheq/ui/screens/homepage/widget/HomeGridItem.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -43,96 +42,12 @@ class _HomePageState extends State<HomePage> {
                 mainAxisSpacing: 8),
             itemCount: cont.albumsWithImages.length,
             itemBuilder: (context, index) {
-              var item = cont.albumsWithImages[index];
-              return ClipRRect(
-                borderRadius: BorderRadius.circular(6),
-                child: Stack(
-                  fit: StackFit.expand,
-                  children: [
-                    Hero(
-                      tag: item.lastImage.path.toString(),
-                      child: Image.file(
-                        item.lastImage,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    Container(
-                      color: Colors.black
-                          .withOpacity(0.4), // Adjust opacity as needed
-                    ),
-                    Positioned(
-                      bottom: 8.0,
-                      left: 8.0,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            item.name,
-                            style: CustomTextStyle.of(context)
-                                .titleBoldStyle(color: Colors.white)
-                                .copyWith(fontSize: 20),
-                          ),
-                          Text(
-                            "${item.totalImage} Photos",
-                            style: CustomTextStyle.of(context)
-                                .mediumTitleBoldStyle(color: Colors.white)
-                                .copyWith(fontSize: 14),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+              var album = cont.albumsWithImages[index];
+              return HomePageGridItem(
+                item: album,
               );
             },
           );
         }));
   }
-}
-
-Future<List<AlbumWithLastImage>> fetchAlbumsWithLastImages() async {
-  List<AlbumWithLastImage> albumsWithImages = [];
-
-  // Get the external storage directory
-  late Directory storageDirectory;
-
-  if (Platform.isAndroid) {
-    storageDirectory = Directory('/storage/emulated/0/DCIM');
-  } else if (Platform.isIOS) {
-    storageDirectory = Directory('/DCIM');
-  } else {
-    throw UnsupportedError('Unsupported platform');
-  }
-
-  if (await storageDirectory.exists()) {
-    // List all directories in the DCIM directory
-    List<FileSystemEntity> entities =
-        storageDirectory.listSync(followLinks: false);
-
-    for (FileSystemEntity entity in entities) {
-      if (entity is Directory) {
-        List<FileSystemEntity> albumContents =
-            entity.listSync(followLinks: false);
-
-        // Filter out directories and get only image files
-        List<File> images =
-            albumContents.whereType<File>().cast<File>().toList();
-
-        if (images.isNotEmpty) {
-          // Sort images by last modified date and get the last one
-          images.sort(
-              (a, b) => b.lastModifiedSync().compareTo(a.lastModifiedSync()));
-          File lastImage = images.first;
-
-          albumsWithImages.add(AlbumWithLastImage(
-              album: entity,
-              lastImage: lastImage,
-              name: entity.path.split('/').last,
-              totalImage: images.length));
-        }
-      }
-    }
-  }
-
-  return albumsWithImages;
 }
---
+++ lib/ui/screens/homepage/widget/HomeGridItem.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '../../../../models/AlbumWithLastImage.dart';
+import '../../../common/textStyle.dart';
+
+class HomePageGridItem extends StatelessWidget {
+  final AlbumWithLastImage item;
+
+  const HomePageGridItem({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(6),
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.file(
+            item.lastImage,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.4), // Adjust opacity as needed
+          ),
+          Positioned(
+            bottom: 8.0,
+            left: 8.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  item.name,
+                  style: CustomTextStyle.of(context)
+                      .titleBoldStyle(color: Colors.white)
+                      .copyWith(fontSize: 20),
+                ),
+                Text(
+                  "${item.totalImage} Photos",
+                  style: CustomTextStyle.of(context)
+                      .mediumTitleBoldStyle(color: Colors.white)
+                      .copyWith(fontSize: 14),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
